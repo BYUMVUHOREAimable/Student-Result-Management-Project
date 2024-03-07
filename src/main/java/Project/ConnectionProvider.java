2b2e045f81e44c27c9a7e1f3e94fbc7dd36b2a1b
BYUMVUHOREAimable <aimablebyumvuhore@gmail.com>
2024-03-07 11:26:29 +0200

    first commint

---
+++ src/main/java/Project/ConnectionProvider.java
@@ -0,0 +1,20 @@
+package Project;
+import java.sql.*;
+
+public class ConnectionProvider {
+
+	public static Connection getCon() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project1","root","");
+			return con;
+		}
+		catch(Exception e) 
+		{
+		System.out.println(e);
+		return null;
+			
+		}
+	}
+	
+}
